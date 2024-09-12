aae11ced4dfc75890427d0c8b1df6ab8484b207e
AliElSaleh <elsaleh78@gmail.com>
2024-09-13 01:49:06 +1000

    delete accidental code

--- physx/source/physx/src/NpFactory.cpp
+++ physx/source/physx/src/NpFactory.cpp
@@ -98,9 +98,7 @@ void NpFactory::release()
 void NpFactory::createInstance()
 {
 	PX_ASSERT(!mInstance);
-printf("yo\n");
 	mInstance = PX_NEW(NpFactory)();
-printf("yo\n");
 }
 
 void NpFactory::destroyInstance()
